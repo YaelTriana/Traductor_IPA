2e9d8a69eba02272fb74735c5ed1ccc37f76e320
OmarZamoranoGarcia <omarchucho13Zago0413@hotmail.com>
2026-03-18 22:44:40 -0700

    Convertir submodulo en carpeta normal

---
+++ traductor-ipa/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <>
+    <div>Landing page</div>
+    <p>Aqui va el traductor como componente</p>
+    </>
+  );
+}
